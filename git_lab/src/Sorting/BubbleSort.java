167474d2bcda693cb1ee7592fe3651e9882f43ce
betty_peng <betty_peng@brown.edu>
2016-03-17 18:57:28 -0400

    bubble

--- git_lab/src/Sorting/BubbleSort.java
+++ git_lab/src/Sorting/BubbleSort.java
@@ -1,26 +1,28 @@
-package Sorting;
 public class BubbleSort {
-	/**
-	 * bubbleSort				sorts an array of integers using bubble sort
-	 * 
-	 * @param unsortedArray		an unsorted array of integers
-	 * @return					the input array, sorted least to greatest
-	 */
-	public static int[] bubbleSort(int[] num) {
-	  int j;
-    boolean swap = true;  
-    int temp;   
-    while (swap){
-      swap= false;    
-      for( j=0; j < num.length -1; j++ ){
-        if ( num[ j ] > num[j+1] ){
-          temp = num[ j ];                
-          num[ j ] = num[ j+1 ];
-          num[ j+1 ] = temp;
-          swap = true;              
-        } 
-      } 
-    } 
-		return num;
-	}
+  /**
+   * bubbleSort sorts an array of integers using bubble sort
+   * 
+   * @param unsortedArray
+   *          an unsorted array of integers
+   * @return the input array, sorted least to greatest
+   */
+  public static int[] bubbleSort(int[] unsortedArray) {
+    // TODO: S1 implements bubble sort
+    int i;
+    int temp;
+    boolean doSwap = true;
+    while (doSwap) {
+      doSwap = false;
+      for (i = 0; i < unsortedArray.length - 1; i++) {
+        if (unsortedArray[i] > unsortedArray[i + 1]) {
+          temp = unsortedArray[i];
+          unsortedArray[i] = unsortedArray[i + 1];
+          unsortedArray[i + 1] = temp;
+          doSwap = true;
+        }
+      }
+    }
+
+    return unsortedArray;
+  }
 }
