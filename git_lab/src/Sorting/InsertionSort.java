9543fcaa270c009e3e76dbbfb3c48de9e234ef27
Katie Liu <katie.z.liu@gmail.com>
2016-03-17 18:59:07 -0400

    insertion sort

--- git_lab/src/Sorting/InsertionSort.java
+++ git_lab/src/Sorting/InsertionSort.java
@@ -1,15 +1,22 @@
 
 public class InsertionSort {
-  /**
-   * insertionSort sorts an array of integers using insertion sort
-   * 
-   * @param unsortedArray
-   *          an unsorted array of integers
-   * @return the input array, sorted least to greatest
-   */
-  public static int[] insertionSort(int[] unsortedArray) {
-    // TODO: S2 implements insertion sort
-
-    return null;
-  }
+	/**
+	 * insertionSort			sorts an array of integers using insertion sort
+	 * 
+	 * @param unsortedArray		an unsorted array of integers
+	 * @return					the input array, sorted least to greatest
+	 */
+	public static int[] insertionSort(int[] unsortedArray) {
+		//TODO: S2 implements insertion sort
+	    int j;
+	    for (int i = 1; i < unsortedArray.length; i++){
+	        int curr = unsortedArray[i];
+	        for (j = i-1; (j>=0) && (unsortedArray[j] < curr); j--){
+	            unsortedArray[j+1] = unsortedArray[j];
+	        }
+	        unsortedArray[j+1] = curr;
+	    }
+	    
+		return unsortedArray;
+	}
 }
